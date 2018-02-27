eaca5c13406b55e99974a0d5364cd0c0d5242d46
KivenLing <KivenL96@163.com>
2018-02-27 11:21:38 +0800

    update O(n)way to solve problem

--- src/arrayproblem/MinimumSizeSubarraySum.java
+++ src/arrayproblem/MinimumSizeSubarraySum.java
@@ -17,7 +17,7 @@ public class MinimumSizeSubarraySum {
         int size = nums.length;
         if (size < 1)//防止传入空数组
             return 0;
-        int[] beforeSum = new int[size];//计算前几项数据和
+        int[] beforeSum = new int[size];//记录nums[start ... end]的和
         beforeSum[0] = nums[0];
         int minLength = 0;//初始化返回长度
         int start = 0;
@@ -30,7 +30,7 @@ public class MinimumSizeSubarraySum {
                  if (end > 0)
                      beforeSum[end] = beforeSum[end - 1] + nums[end];
                  int length = end - start + 1;
-                 if (beforeSum[end] > s &&
+                 if (beforeSum[end] >= s &&
                          (minLength == 0 || end - start + 1 < minLength)) {
                      minLength = end - start + 1;
                      break;
@@ -42,9 +42,37 @@ public class MinimumSizeSubarraySum {
 
     //滑动窗口
     public static int minSubArrayLenImprove(int s, int[] nums){
+        int size = nums.length;
+        if (size < 1)//防止传入空数组
+            return 0;
+        //这里可以值用一个值记录，不用数组记录的。
+        int[] beforeSum = new int[size];//记录nums[start ... end]的和
+        beforeSum[0] = nums[0];
+        int minLength = 0;//初始化返回长度
+        int start = 0;
+        int end = 0;
+        while (end >= start && end < nums.length){
+            int length = end - start + 1;
+            if (beforeSum[end] >= s) {//nums[start ... end] >= s
+                if (minLength == 0 || length < minLength)
+                    minLength = length;
+                beforeSum[end] -= nums[start];//更新beforeSum的值
+                start++;
+            }else{//nums[start ... end] < s,此时end指针向前移动
+                end++;
+                if (end >= nums.length)
+                    break;
+                beforeSum[end] = beforeSum[end - 1] + nums[end];
+            }
+        }
+        return minLength;
 
     }
     public static void main(String[] args){
-
+        int s = 7;
+        int[] nums = {1, 3, 4, 4, 3, 7};
+        int length1 = minSubArrayLen(s, nums);
+        int length2 = minSubArrayLenImprove(s, nums);
+        System.out.println(length1 + " " + length2);
     }
 }
