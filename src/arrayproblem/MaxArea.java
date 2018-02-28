979589f043e806e30c15c75e48c7a0e10a8ee2e8
KivenLing <KivenL96@163.com>
2018-02-28 11:43:03 +0800

    ID:11 solved

--- src/arrayproblem/MaxArea.java
+++ src/arrayproblem/MaxArea.java
@@ -1,5 +1,6 @@
 package arrayproblem;
 /*
+ID:11
 Given n non-negative integers a1, a2, ..., an,
 where each represents a point at coordinate (i, ai).
 n vertical lines are drawn such that the two endpoints
