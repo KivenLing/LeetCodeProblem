895e152b2e9fcb49739df17643e80987ad4d58c7
KivenLing <KivenL96@163.com>
2018-03-14 14:23:32 +0800

    ID：149 one test case wrong

--- src/hashtable/MaxPointsOnLine.java
+++ src/hashtable/MaxPointsOnLine.java
@@ -1,5 +1,8 @@
 package hashtable;
 
+import util.Point;
+import util.ReadCase;
+
 import java.util.Arrays;
 import java.util.HashSet;
 import java.util.Set;
@@ -9,7 +12,9 @@ ID:149
 Given n points on a 2D plane, find the maximum
 number of points that lie on the same straight line.
  */
+
 public class MaxPointsOnLine {
+    //有一个测试用例通不过不知道为什么
     public static int maxPoints(Point[] points) {
         int maxP = 0;
         int size = points.length;
@@ -38,19 +43,18 @@ public class MaxPointsOnLine {
                 }
                 visited[j] = true;
                 sumOfPoint++;
-                Line line = lineOfTwoPoints(points[i], points[j]);
+                Line line = new Line(points[i], points[j]);
                 if (lineRecord.contains(line))
                     continue;
                 lineRecord.add(line);
                 for (int k = j + 1; k < size; k++) {
-                    while (k < size && points[k].x == points[j].x &&
-                            points[k].y == points[j].y){
+                    if (points[k].x == points[i].x &&
+                            points[k].y == points[i].y){
                         visited[k] = true;
-                        k++;
                         sumOfPoint++;
+                        continue;
                     }
-                    if (k < size &&
-                            line.equals(lineOfTwoPoints(points[j],points[k]))) {
+                    if (line.equals(new Line(points[i],points[k]))) {
                         visited[k] = true;
                         sumOfPoint++;
                     }
@@ -62,53 +66,59 @@ public class MaxPointsOnLine {
         return maxP;
     }
 
-    public static Line lineOfTwoPoints(Point a, Point b){
-        Line line = new Line();
-        if (a.x == b.x){
-            line.slope = Integer.MAX_VALUE;
-            line.intercept = a.x;
-        }else {
-            line.slope = (a.y - b.y) / (a.x - b.x);
-            line.intercept = (b.y * a.x - a.y * b.x) / (a.x - b.x);
-        }
-        return line;
-    }
-
     public static void main(String[] args){
-        Point p2 = new Point(-8, -582);
-        Point p4 = new Point(-9,-651);
-        Point p5 = new Point(9, 591);
-        Point p1 = new Point(-4, -4);
-        Point p3 = new Point(-3, 3);
-        Point[] poins = {p1, p2, p3, p4, p5};
+        String cases = "[40,-23],[9,138],[429,115],[50,-17],[-3,80]," +
+                "[-10,33],[5,-21],[-3,80],[-6,-65],[-18,26],[-6,-65]," +
+                "[5,72],[0,77],[-9,86],[10,-2],[-8,85],[21,130],[18,-6]," +
+                "[-18,26],[-1,-15],[10,-2],[8,69],[-4,63],[0,3],[-4,40]," +
+                "[-7,84],[-8,7],[30,154],[16,-5],[6,90],[18,-6],[5,77]," +
+                "[-4,77],[7,-13],[-1,-45],[16,-5],[-9,86],[-16,11],[-7,84]," +
+                "[1,76],[3,77],[10,67],[1,-37],[-10,-81],[4,-11],[-20,13]," +
+                "[-10,77],[6,-17],[-27,2],[-10,-81],[10,-1],[-9,1],[-8,43]," +
+                "[2,2],[2,-21],[3,82],[8,-1],[10,-1],[-9,1],[-12,42],[16,-5]," +
+                "[-5,-61],[20,-7],[9,-35],[10,6],[12,106],[5,-21],[-5,82],[6,71]," +
+                "[-15,34],[-10,87],[-14,-12],[12,106],[-5,82],[-46,-45],[-4,63]," +
+                "[16,-5],[4,1],[-3,-53],[0,-17],[9,98],[-18,26],[-9,86],[2,77]," +
+                "[-2,-49],[1,76],[-3,-38],[-8,7],[-17,-37],[5,72],[10,-37],[-4,-57]," +
+                "[-3,-53],[3,74],[-3,-11],[-8,7],[1,88],[-12,42],[1,-37],[2,77],[-6,77]," +
+                "[5,72],[-4,-57],[-18,-33],[-12,42],[-9,86],[2,77],[-8,77],[-3,77]," +
+                "[9,-42],[16,41],[-29,-37],[0,-41],[-21,18],[-27,-34],[0,77],[3,74]," +
+                "[-7,-69],[-21,18],[27,146],[-20,13],[21,130],[-6,-65],[14,-4],[0,3]," +
+                "[9,-5],[6,-29],[-2,73],[-1,-15],[1,76],[-4,77],[6,-29]";
+        String cases2 = "[0,0],[1,65536],[65536,0]";
+        Point[] poins = ReadCase.readPoints(cases);
         System.out.println(maxPoints(poins));
     }
 }
-class Point {
-      int x;
-      int y;
-      Point() {
-          x = 0;
-          y = 0;
-      }
-      Point(int a, int b) {
-          x = a;
-          y = b;
-      }
-}
-
 class Line{
-    double slope;//斜率
-    double intercept;//截距
-    Line(){}
-    Line(double a, double b){
-        slope = a;
-        intercept = b;
+    public long a;
+    public long b;
+    public long c;
+
+    public Line() {}
+
+    public Line(long a, long b, long c) {
+        this.a = a;
+        this.b = b;
+        this.c = c;
     }
 
+    public Line (Point p1, Point p2){
+        a = p2.y - p1.y;
+        b = p1.x - p2.x;
+        c = (long)p2.x * p1.y - (long)p1.x * p2.y;
+    }
     @Override
     public boolean equals(Object obj) {
-        return  obj instanceof Line &&
-                slope == ((Line) obj).slope && intercept == ((Line) obj).intercept;
+        if (!(obj instanceof Line))
+            return false;
+        if (a * ((Line) obj).b == ((Line) obj).a * b &&
+                a * ((Line) obj).c == ((Line) obj).a * c &&
+                b * ((Line) obj).c == ((Line) obj).b * c)
+            return true;
+        return false;
     }
 }
+
+
+
