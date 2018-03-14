7c5dd1373aa1362f2d64319b7c85d3f9b67ecb7a
KivenLing <KivenL96@163.com>
2018-03-14 16:17:14 +0800

    ID：217 219 220 solved

--- src/hashtable/ContainsNearbyDuplicate.java
+++ src/hashtable/ContainsNearbyDuplicate.java
@@ -2,10 +2,7 @@ package hashtable;
 
 import com.sun.javafx.image.IntPixelGetter;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 /*
 ID:219
@@ -45,4 +42,65 @@ public class ContainsNearbyDuplicate {
         }
         return false;
     }
+
+    /*
+    ID:217
+    Given an array of integers, find if the array contains any duplicates.
+    Your function should return true if any value appears at least twice
+    in the array, and it should return false if every element is distinct.
+     */
+    public static boolean containsDuplicate(int[] nums) {
+        if (nums.length <= 1)
+            return false;
+        Set<Integer> record = new HashSet<>();
+        for (int i : nums) {
+            if (record.contains(i))
+                return true;
+            record.add(i);
+        }
+        return false;
+    }
+
+    /*
+    Given an array of integers, find out whether there are two distinct
+    indices i and j in the array such that the absolute difference
+    between nums[i] and nums[j] is at most t and the absolute difference
+    between i and j is at most k.
+     */
+    //利用sortedtree的函数很简单，还有一种bucket的解法更加好，待跟进
+    public static boolean containsNearbyAlmostDuplicate(int[] nums, int k, int t) {
+        if(k<1 || t<0 || nums==null || nums.length<2) return false;
+
+        SortedSet<Long> set = new TreeSet<Long>();
+
+        for(int j=0; j<nums.length; j++) {
+            SortedSet<Long> subSet =  set.subSet((long)nums[j]-t, (long)nums[j]+t+1);
+            if(!subSet.isEmpty()) return true;
+
+            if(j>=k) {
+                set.remove((long)nums[j-k]);
+            }
+            set.add((long)nums[j]);
+        }
+        return false;
+    }
+    //网上bucket解法
+    public static boolean containsNearbyAlmostDuplicateImprove(int[] nums, int k, int t) {
+        if (k < 1 || t < 0) return false;
+        Map<Long, Long> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            long remappedNum = (long) nums[i] - Integer.MIN_VALUE;
+            long bucket = remappedNum / ((long) t + 1);
+            if (map.containsKey(bucket) || (map.containsKey(bucket - 1) && remappedNum - map.get(bucket - 1) <= t) || (map.containsKey(bucket + 1) && map.get(bucket + 1) - remappedNum <= t)) {
+                return true;
+            }
+            if (map.entrySet().size() >= k) {
+                long lastBucket = ((long) nums[i - k] - Integer.MIN_VALUE) / ((long) t + 1);
+                map.remove(lastBucket);
+            }
+            map.put(bucket, remappedNum);
+        }
+        return false;
+    }
 }
+
