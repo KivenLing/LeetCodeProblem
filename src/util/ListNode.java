2f88eded421128d2d988d47ac5b90deab9ef01cf
KivenLing <KivenL96@163.com>
2018-03-16 09:47:29 +0800

    ID 328 solved

--- src/util/ListNode.java
+++ src/util/ListNode.java
@@ -3,7 +3,7 @@ package util;
 public class ListNode {
     public int val;
     public ListNode next;
-    ListNode(int x) { val = x; }
+    public ListNode(int x) { val = x; }
 
     // 根据n个元素的数组arr创建一个链表
     // 使用arr为参数，创建另外一个ListNode的构造函数
