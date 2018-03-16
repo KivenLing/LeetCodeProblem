f7e82dc6402504b0148afb0c43c9fd3409ea700d
KivenLing <KivenL96@163.com>
2018-03-16 16:23:47 +0800

    ID : 82 83 solved

--- src/linkedlist/RemoveDuplicates.java
+++ src/linkedlist/RemoveDuplicates.java
@@ -2,15 +2,19 @@ package linkedlist;
 
 import util.ListNode;
 /*
-ID : 83
-Given a sorted linked list, delete all duplicates
-such that each element appear only once.
-
-For example,
-Given 1->1->2, return 1->2.
-Given 1->1->2->3->3, return 1->2->3.
+ID: 82, 83
  */
 public class RemoveDuplicates {
+    /*
+    ID : 83
+    Given a sorted linked list, delete all duplicates
+    such that each element appear only once.
+
+    For example,
+    Given 1->1->2, return 1->2.
+    Given 1->1->2->3->3, return 1->2->3.
+    */
+
     public static ListNode deleteDuplicates(ListNode head) {
         ListNode cur = head;
         while (cur != null){
@@ -21,5 +25,35 @@ public class RemoveDuplicates {
             cur = cur.next;
         }
         return head;
+    }
+
+    /*
+    ID : 82
+    Given a sorted linked list, delete all nodes that have duplicate
+    numbers, leaving only distinct numbers from the original list.
+
+    For example,
+    Given 1->2->3->3->4->4->5, return 1->2->5.
+    Given 1->1->1->2->3, return 2->3.
+     */
+    public static ListNode deleteDuplicatesII(ListNode head) {
+        ListNode dummyHead = new ListNode();
+        dummyHead.next = head;
+        ListNode pre = dummyHead;
+        ListNode cur = head;
+        while (cur != null){
+            if (cur.next != null && cur.val == cur.next.val) {
+                while (cur.next != null && cur.val == cur.next.val) {
+                    cur = cur.next;
+                }
+                //cur 指向 相同元素最后一个
+                cur = cur.next;
+                pre.next = cur;
+            }else {
+                pre = cur;
+                cur = cur.next;
+            }
+        }
+        return dummyHead.next;
     }
 }
