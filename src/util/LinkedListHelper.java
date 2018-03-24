6e305731b89810ee0a8c9db8f5caf57fd53cfc72
KivenLing <KivenL96@163.com>
2018-03-24 14:08:46 +0800

    add linkedList Helper method

---
+++ src/util/LinkedListHelper.java
@@ -0,0 +1,24 @@
+package util;
+
+public class LinkedListHelper {
+    private LinkedListHelper(){}
+
+    //返回这个链表第i个节点
+    public static ListNode getnthNodeOfList(ListNode head, int n){
+        int size = 1;
+        while (size < n && head != null){
+            size++;
+            head = head.next;
+        }
+        return head;
+    }
+    
+    public static int sizeOfNode(ListNode head){
+        int size = 0;
+        while (head != null){
+            size++;
+            head = head.next;
+        }
+        return size;
+    }
+}
