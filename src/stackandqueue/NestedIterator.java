b1b414a9db46d1691bc538e6d3a626e7e371ab8e
KivenLing <KivenL96@163.com>
2018-03-26 11:38:55 +0800

    ID 341 solved

---
+++ src/stackandqueue/NestedIterator.java
@@ -0,0 +1,86 @@
+package stackandqueue;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+/**
+ * ID：341
+ * Given a nested list of integers, implement an iterator to flatten it.
+
+ Each element is either an integer, or a list -- whose elements may also
+ be integers or other lists.
+
+ Example 1:
+ Given the list [[1,1],2,[1,1]],
+
+ By calling next repeatedly until hasNext returns false, the order of
+ elements returned by next should be: [1,1,2,1,1].
+
+ Example 2:
+ Given the list [1,[4,[6]]],
+
+ By calling next repeatedly until hasNext returns false, the order of
+ elements returned by next should be: [1,4,6].
+ * // This is the interface that allows for creating nested lists.
+ * // You should not implement it, or speculate about its implementation
+ * public interface NestedInteger {
+ *
+ *     // @return true if this NestedInteger holds a single integer,
+ *     //rather than a nested list.
+ *     public boolean isInteger();
+ *
+ *     // @return the single integer that this NestedInteger holds,
+ *     //if it holds a single integer
+ *     // Return null if this NestedInteger holds a nested list
+ *     public Integer getInteger();
+ *
+ *     // @return the nested list that this NestedInteger holds,
+ *     //if it holds a nested list
+ *     // Return null if this NestedInteger holds a single integer
+ *     public List<NestedInteger> getList();
+ * }
+ */
+public class NestedIterator implements Iterator<Integer> {
+
+    private List<Integer> elements;
+    private Iterator iterator;
+
+    public NestedIterator(List<NestedInteger> nestedList) {
+        elements = new ArrayList<>();
+        parder(nestedList);
+        iterator = elements.iterator();
+    }
+
+    @Override
+    public Integer next() {
+        return (Integer)iterator.next();
+    }
+
+    @Override
+    public boolean hasNext() {
+        return iterator.hasNext();
+    }
+
+    private void parder(List<NestedInteger> nestedList){
+        for(NestedInteger nexted : nestedList){
+            if(nexted.isInteger()){
+                elements.add(nexted.getInteger());
+            }else{
+                parder(nexted.getList());
+            }
+        }
+    }
+}
+  interface NestedInteger {
+    // @return true if this NestedInteger holds a single integer, rather than a nested list.
+    public boolean isInteger();
+
+    // @return the single integer that this NestedInteger holds, if it holds a single integer
+    // Return null if this NestedInteger holds a nested list
+    public Integer getInteger();
+
+    // @return the nested list that this NestedInteger holds, if it holds a nested list
+    // Return null if this NestedInteger holds a single integer
+    public List<NestedInteger> getList();
+  }
