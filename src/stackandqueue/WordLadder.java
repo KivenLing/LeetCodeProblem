cbabb70adbeb231227a6cb55f43aaddea6c7c130
KivenLing <KivenL96@163.com>
2018-04-09 19:57:37 +0800

    ID 127 solved well ID 126 todo

--- src/stackandqueue/WordLadder.java
+++ src/stackandqueue/WordLadder.java
@@ -67,32 +67,129 @@ public class WordLadder {
         return 0;
     }
 
-    //从end推begin 稍微优化了些 726ms
+    //从end推begin 稍微优化了些 488ms
     public static int ladderLengthImprove(String beginWord, String endWord, List<String> wordList) {
-        if (!wordList.contains(endWord))
+        Set<String> words = new HashSet<>(wordList);
+        if (!words.contains(endWord))
             return 0;
         List<Pair<String, Integer>> queue = new LinkedList<>();
-        Set<String> visitedWord = new HashSet<>();
         queue.add(new Pair<>(endWord, 1));
-        visitedWord.add(endWord);
         while (!queue.isEmpty()){
             String word = queue.get(0).getKey();
             int length = queue.get(0).getValue();
             queue.remove(0);
             if (isOnePosDiff(word, beginWord))
                 return length + 1;
-            for (String str : wordList){
-                if (visitedWord.contains(str))
-                    continue;
+            Set<String> removeSet = new HashSet<>();
+            for (String str : words){
                 if (isOnePosDiff(word, str)){
                     queue.add(new Pair<>(str, length + 1));
-                    visitedWord.add(str);
+                    removeSet.add(str);
                 }
             }
+            words.removeAll(removeSet);
         }
         return 0;
     }
 
+    //在寻找下一条路径进行了优化，对每个word，对他每个位置进行a-z的替换
+    //时间效率96ms
+    public static int ladderLengthImproveII(String beginWord, String endWord, List<String> wordList) {
+        Set<String> words = new HashSet<>(wordList);
+        if (!words.contains(endWord))
+            return 0;
+        words.add(beginWord);
+        Set<String> fromEnd = new HashSet<>();
+        fromEnd.add(endWord);
+        int step = 1;
+        Set<String> nextStep = new HashSet<>();
+        while (!fromEnd.isEmpty()){
+            nextStep.clear();
+            for (String word : fromEnd) {
+                char[] origin = word.toCharArray();
+                for (int i = 0; i < word.length(); i++) {
+                    char c = origin[i];
+                    for (char diff = 'a'; diff <= 'z'; diff++){
+                        if (diff == c)
+                            continue;
+                        origin[i] = diff;
+                        String nextStr = String.valueOf(origin);
+                        if (words.contains(nextStr)){
+                            if (nextStr.equals(beginWord))
+                                return step + 1;
+                            words.remove(nextStr);
+                            nextStep.add(nextStr);
+                        }
+                        origin[i] = c;
+                    }
+                }
+            }
+            fromEnd.clear();
+            fromEnd.addAll(nextStep);
+            step++;
+        }
+        return 0;
+    }
+
+    //对上一个方法进行优化
+    //从头尾两边进行层序，哪边一层节点数少，就从哪边层序遍历一层，
+    // 这样就减少了许多不必要遍历的开支
+    public static int ladderLengthImproveIII(String beginWord, String endWord, List<String> wordList){
+        Set<String> words = new HashSet<>(wordList);
+        words.add(beginWord);
+        if (!words.contains(endWord)) {
+            return 0;
+        }
+        Set<String> fromBegin = new HashSet<>();
+        Set<String> fromEnd = new HashSet<>();
+        //两个set只保留这一层的节点
+        fromBegin.add(beginWord);
+        fromEnd.add(endWord);
+        int length = 1;
+        while(!fromBegin.isEmpty() && !fromEnd.isEmpty()) {
+            //判断size大小，让节点数少的进行层序遍历一层，这样就减少了不必要的层序遍历
+            if (fromBegin.size() < fromEnd.size()) {
+                if (convert(fromBegin, fromEnd, words)) {
+                    return length + 1;
+                }
+            }else {
+                if (convert(fromEnd, fromBegin, words)) {
+                    return length + 1;
+                }
+            }
+            length++;
+        }
+        return 0;
+    }
+
+    //对begin进行层序遍历一层，并且将遍历的节点替换上一层
+    private static boolean convert(Set<String> begin, Set<String> next, Set<String> words){
+        Set<String> toAdd = new HashSet<>();
+        for (String word : begin) {
+            char[] chars = word.toCharArray();
+            for (int i = 0; i < chars.length; i++) {
+                char origin = chars[i];
+                for (char c = 'a'; c <= 'z'; c++) {
+                    if (c == origin) {
+                        continue;
+                    }
+                    chars[i] = c;
+                    String nextStr = new String(chars);
+                    if (next.contains(nextStr)) {
+                        return true;
+                    }
+                    if (words.contains(nextStr)) {
+                        words.remove(nextStr);
+                        toAdd.add(nextStr);
+                    }
+                    chars[i] = origin;
+                }
+            }
+        }
+        begin.clear();
+        begin.addAll(toAdd);
+        return false;
+    } 
 
 
     //只有一个字母不同，但是顺序可以不同，与题目不符合
@@ -128,6 +225,27 @@ public class WordLadder {
     //bfs 解法可以参考：
     //https://github.com/liuyubobobo/Play-Leetcode/tree/master/0127-Word-Ladder/java-0127/src
 
+    /**
+     * ID 126
+     * the same to 127
+     *
+     * For example,
+     *
+     * Given:
+     * beginWord = "hit"
+     * endWord = "cog"
+     * wordList = ["hot","dot","dog","lot","log","cog"]
+     * Return
+     * [
+     * ["hit","hot","dot","dog","cog"],
+     * ["hit","hot","lot","log","cog"]
+     * ]
+     */
+    public static List<List<String>> findLadders(String beginWord, String endWord, List<String> wordList) {
+        List<List<String>> ans = new ArrayList<>();
+
+        return ans;
+    }
     public static void main(String[] args) {
         String begin = "hit";
         String end = "cog";
@@ -139,64 +257,6 @@ public class WordLadder {
         wordList.add("log");
         wordList.add("cog");
         wordList.add("los");
-        System.out.println(ladderLengthImprove(begin, end, wordList));
+        System.out.println(ladderLengthImproveII(begin, end, wordList));
     }
 }
-/*
-leetcode java时间最优解
-class Solution {
-
-    private static class WordNode {
-        private String word;
-        private int numSteps;
-        private WordNode(String word, int numSteps) {
-            this.word = word;
-            this.numSteps = numSteps;
-        }
-    }
-
-    public int ladderLength(String beginWord, String endWord, List<String> wordList) {
-        Set<String> words = new HashSet<>(wordList);
-        if (!words.contains(endWord)) return 0;
-        Set<String> fromBegin = new HashSet<>();
-        Set<String> fromEnd = new HashSet<>();
-        fromBegin.add(beginWord);
-        fromEnd.add(endWord);
-        int dist = 1;
-
-        while (!fromBegin.isEmpty() && !fromEnd.isEmpty()) {
-            if (fromBegin.size() < fromEnd.size()) {
-                if (convert(fromBegin, fromEnd, words)) return dist + 1;
-            }else {
-                if (convert(fromEnd, fromBegin, words)) return dist + 1;
-            }
-            dist++;
-        }
-
-        return 0;
-    }
-
-    private boolean convert(Set<String> cur, Set<String> next, Set<String> words) {
-        Set<String> toAdd = new HashSet<>();
-        for (String word : cur) {
-            char[] chars = word.toCharArray();
-            for (int j = 0; j < chars.length; j++) {
-                char origin = chars[j];
-                for (char c = 'a'; c <= 'z'; c++) {
-                    chars[j] = c;
-                    String newStr = new String(chars);
-                    if (next.contains(newStr)) return true;
-                    if (words.contains(newStr)) {
-                        words.remove(newStr);
-                        toAdd.add(newStr);
-                    }
-                }
-                chars[j] = origin;
-            }
-        }
-        cur.clear();
-        cur.addAll(toAdd);
-        return false;
-    }
-}
-*/
