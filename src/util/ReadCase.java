9fad8baf08d948ea2dcf2b388cf2da8a89eb86a7
KivenLing <KivenL96@163.com>
2018-04-13 12:12:33 +0800

    add read testcase from file

--- src/util/ReadCase.java
+++ src/util/ReadCase.java
@@ -1,4 +1,10 @@
 package util;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
 /*
 用于读入输出测试数据
  */
@@ -25,5 +31,34 @@ public class ReadCase {
             p[i] = new Point(x, y);
         }
         return p;
+    }
+
+    public static List<String> readTestCase(String filename){
+        String cases = readCaseByFile(filename).replaceAll("\"", "");
+        String[] strs = cases.split(",");
+        List<String> testcases = new ArrayList<>(Arrays.asList(strs));
+        return testcases;
+    }
+
+    private static String readCaseByFile(String fileName){
+        File file = new File(fileName);
+        StringBuffer stringBuffer = new StringBuffer();
+        Reader reader = null;
+        try {
+            reader = new InputStreamReader(new FileInputStream(file));
+            int tempChar;
+            while ((tempChar = reader.read()) != -1){
+                stringBuffer.append((char)tempChar);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+        return stringBuffer.toString();
+    }
+
+    public static void main(String[] args) {
+        String filename = "testcase.txt";
+        List<String> cases = readTestCase(filename);
+        System.out.println(cases);
     }
 }
