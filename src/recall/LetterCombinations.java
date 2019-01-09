9efbb707a727e5f9844261d725b970015456cb08
KivenLing <KivenL96@163.com>
2019-01-09 21:59:04 +0800

    ID:17 93 solved

--- src/recall/LetterCombinations.java
+++ src/recall/LetterCombinations.java
@@ -6,6 +6,7 @@ import java.util.List;
 /**
  * @author Kiven Ling
  * 2018/5/14 14:58
+ * ID: 17 93
  */
 public class LetterCombinations {
     private static final String[] DIGIT_TO_LETTER = {
@@ -24,10 +25,6 @@ public class LetterCombinations {
     public static List<String> letterCombinations(String digits){
         if (digits == null || digits.length() <= 0){
             return null;
-        }
-        List<String> letters = new ArrayList<>();
-        for (char digit : digits.toCharArray()) {
-            String toLetter = DIGIT_TO_LETTER[digit - '1'];
         }
         //todo
         return null;
@@ -56,6 +53,63 @@ public class LetterCombinations {
         String letters = DIGIT_TO_LETTER[digits.charAt(index) - '0'];
         for (int i = 0; i < letters.length(); i++) {
             findCombination(res, digits, index + 1, pre + letters.charAt(i));
+        }
+    }
+
+    /**ID: 93
+     * Given a string containing only digits, restore it by returning
+     * all possible valid IP address combinations.
+     *
+     * Example:
+     * Input: "25525511135"
+     * Output: ["255.255.11.135", "255.255.111.35"]
+     */
+    public static List<String> restoreIpAddresses(String s) {
+        List<String> res = new ArrayList<>();
+        if (s.length() == 0 || s.length() > 12)
+            return res;
+        findIp(res, s, 0, 1, new StringBuffer());
+        return res;
+    }
+
+    private static void findIp(List<String> res, String ips, int index, int size,StringBuffer preIp){
+        //可用ips[index .. end]
+        int ipLen = ips.length();
+        if (size == 4){
+            String ipPart = ips.substring(index, ipLen);
+            if (isLegalIpAddrPart(ipPart)){
+                preIp.append(ipPart);
+                res.add(preIp.toString());
+            }
+            return;
+        }
+        //ip 字段不能大于3，且不能到末尾
+        //i 表示截断ips，ips[index .. i)
+        for (int i = index + 1; i <= index + 3 && i < ipLen; i++) {
+            //若果截断后，ips长度大于不可能构成ip地址
+            if ((ipLen - i) > (4 - size) * 3) continue;
+            String ipPart = ips.substring(index, i);//ips[index .. i)
+            if (isLegalIpAddrPart(ipPart)){
+                StringBuffer preIpAppend = new StringBuffer(preIp);
+                preIpAppend.append(ipPart).append('.');
+                findIp(res, ips, i, size + 1, preIpAppend);
+            }
+        }
+    }
+
+    //验证单个ip片段是否合理
+    private static boolean isLegalIpAddrPart(String ipPart){
+        //ip片段不能已零开头的多位数
+        if (ipPart.charAt(0) == '0' && ipPart.length() > 1) return false;
+        int ipPartInt = Integer.parseInt(ipPart);
+        return ipPartInt >= 0 && ipPartInt <= 255;
+    }
+
+    public static void main(String[] args) {
+        String s = "0000";
+        List<String> ips = restoreIpAddresses(s);
+        for (String ip : ips) {
+            System.out.println(ip);
         }
     }
 }
